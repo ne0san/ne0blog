4ef55d489dfb825a217084a631a7ab2dd9ada46e
neosan <nilva3465@gmail.com>
2025-06-22 08:46:44 +0900

    rssのタイトルと説明を修正

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,15 +1,13 @@
-import rss, {pagesGlobToRssItems} from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async (context) => {
-  return rss({
-    title: 'ねおぶろぐ',
-    description: 'テックも雑談もごちゃ混ぜのブログ',
-    site: context.site!,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./posts/*.{md,mdx}'),
-    ),
-    stylesheet: './rss/styles.xsl',
-    customData: `<language>ja-jp</language>`,
-  });
-}
+	return rss({
+		title: "ねおさん's ろぐ",
+		description: 'テックも雑談もごちゃ混ぜの個人ブログ',
+		site: context.site!,
+		items: await pagesGlobToRssItems(import.meta.glob('./posts/*.{md,mdx}')),
+		stylesheet: './rss/styles.xsl',
+		customData: `<language>ja-jp</language>`
+	});
+};
